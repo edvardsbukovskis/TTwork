79062f757734b793baf9fe12c95396fc020e137b
Edvards Bukovskis <edvards.bukovskis@gmail.com>
2019-06-12 18:15:58 +0300

    Created databases through migrations

---
+++ database/migrations/2019_06_12_151158_create_atbildes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtbildesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atbildes', function (Blueprint $table) {
+            $table->bigIncrements('atbildes_id')->unique();
+            $table->integer('jautajumi_id')->unsigned();
+            $table->foreign('jautajumi_id')->references('jautajumi_id')->on('jautajumi');
+            $table->integer('users_id')->unsigned();
+            $table->foreign('users_id')->references('users_id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atbildes');
+    }
+}
