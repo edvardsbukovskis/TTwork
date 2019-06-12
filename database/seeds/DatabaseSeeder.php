2bbb4b4052f2edd2b69fea5355151d478eae4053
emkensj <emkensj@gmail.com>
2019-06-12 22:39:54 +0300

    Getting fresh start after crash

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,37 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-          'name'=>'Edvards Bukovskis',
-          'email'=>'edvards.bukovskis@gmail.com',
-          'password'=>bcrypt('Parole123'),
-        ]);
-        DB::table('users')->insert([
-          'name'=>'Janis Emkens',
-          'email'=>'emkensj@gmail.com',
-          'password'=>bcrypt('Parole123'),
-        ]);
-        DB::table('users')->insert([
-          'name'=>'Karlis Roberts Krumins',
-          'email'=>'krk@gmail.com',
-          'password'=>bcrypt('Parole123'),
-        ]);
-        DB::table('anketa')->insert([
-          'description'=>'Jautājums par dzīvi',
-          'nosaukums'=>'Šis ir pirmais jautājums',
-          'users_id'=>'1',
-        ]);
-        DB::table('jautajumi')->insert([
-          'anketa_id'=>'1',
-          'jautajums'=>'Kurā kursā mēs mācāmies (atbildēt ar vārdu)',
-        ]);
-        DB::table('atbildes')->insert([
-          'atbilde'=>'Pirmajā',
-          'jautajumi_id'=>'1',
-          'users_id'=>'2',
-        ]);
-
-
-
+        // $this->call(UsersTableSeeder::class);
     }
 }
