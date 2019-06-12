a5afab65f8f9e48c6798d06d5b5249cf8fd1f2a7
Edvards Bukovskis <edvards.bukovskis@gmail.com>
2019-06-12 13:32:24 +0300

    Initialisaion of materializecss

--- config/app.php
+++ config/app.php
@@ -189,7 +189,8 @@ return [
     */
 
     'aliases' => [
-
+		
+		'MaterializeCSS' => Skydiver\LaravelMaterializeCSS\MaterializeCSS::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
