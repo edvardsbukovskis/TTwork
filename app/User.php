881495b7efa4785a53c335964d1d379350668b78
emkensj <emkensj@gmail.com>
2019-06-12 22:53:20 +0300

    correction

--- app/User.php
+++ app/User.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class user extends Model
 {
     protected $table='users';
+	protected $fillable=['name'];
 
     public function anketa(){
-      return $this->hasMany(App\anketa);
+      return $this->hasMany('App\anketa');
     }
     public function atbildes(){
       return $this->hasMany('App\atbildes');
