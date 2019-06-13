49f1e688c9ac5a5d79af6e5fb838eee3498bdeeb
emkensj <emkensj@gmail.com>
2019-06-13 20:46:49 +0300

    Added new looks

--- resources/views/AnketasAtbildes.blade.php
+++ resources/views/AnketasAtbildes.blade.php
@@ -1,3 +1,27 @@
+<style>
+html, body {
+    background-color: #fff;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 150;
+    height: 100vh;
+    margin: 0;
+}
+
+.links > a {
+    color: #636b6f;
+    padding: 25px;
+    margin: 10px;
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+    text-align: center;
+}
+.links{
+  margin:10px;
+}
+</style>
 <div class="links">
 					<a href="{{ url('/') }}">Sākums</a>
                     <a href="{{ url('/anketa') }}">Anketas</a>
--- resources/views/anketas.blade.php
+++ resources/views/anketas.blade.php
@@ -1,8 +1,34 @@
-<div class="links">
-                    <a href="{{ url('/') }}">Sākums</a>
+<style>
+html, body {
+    background-color: #fff;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 150;
+    height: 100vh;
+    margin: 0;
+}
+
+.links > a {
+    color: #636b6f;
+    padding: 25px;
+    margin: 10px;
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+    text-align: center;
+}
+.links{
+  margin:10px;
+}
+</style>
+
+<div class="links" style="margin:10px;">
+          <a href="{{ url('/') }}">Sākums</a>
 					<a href="{{ url('/anketa/create') }}">Izveidot Anketu</a>
 
-                </div>
+</div>
+
 <div style="margin: 10px; background-color: darkgray; border: 1px solid black;">
 @foreach ($anketas as $anketa)
 <div style="margin: 10px; border: 2px solid black;">
